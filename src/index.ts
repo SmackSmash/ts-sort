73e27f054ae96a23a27839341b3234dba8952e50
Dan Moffat <danmoffat@gmail.com>
2024-04-17 23:48:57 +0100

    Partly implemented bubble sort

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 console.log('Some changes');
+
+const numArr = [5, 0, 10, -1];
+
+const bubbleSort = (array: number[]) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1]) {
+      [array[i], array[i + 1]] = [array[i + 1], array[i]];
+    }
+  }
+  console.log(array);
+};
+
+bubbleSort(numArr);
