ee60e924bdd38d5f37297e3b3139265749a5ce41
Dan Moffat <danmoffat@gmail.com>
2024-04-18 00:05:02 +0100

    Refactored bubblesort to not use a separate variable

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 const numArr = [5, 0, 10, -1];
 
 const bubbleSort = (array: number[]): number[] => {
-  let depth = array.length;
-  for (let j = 0; j < array.length; j++) {
-    for (let i = 0; i < depth; i++) {
-      if (array[i] > array[i + 1]) {
-        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+  for (let i = array.length; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
       }
     }
-    depth--;
   }
   return array;
 };
