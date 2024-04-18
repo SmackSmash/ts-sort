9d89ca62de54a38f84cdfcd3b51e8e4a630a3d12
Dan Moffat <danmoffat@gmail.com>
2024-04-18 16:53:26 +0100

    Tested with large array

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 const numArr = [5, 0, 10, -1];
+const largeNumArr = [
+  55, 41, -51, 0, 78, 74, -81, -72, -49, -2, -54, 8, 38, 81, 25, -24, 35, -40,
+  -66, -50, 62, -75, 97, 86, 66, 47, -56, 20, -16, -47, 45, -84, -30, 40, -63,
+  99, -52, -8, 18, -18, -89, 46, -87, 3, 82, 31, 23, 83, -95, -80
+];
 
 const bubbleSort = (array: number[]): number[] => {
   for (let i = array.length; i > 0; i--) {
@@ -12,3 +17,4 @@ const bubbleSort = (array: number[]): number[] => {
 };
 
 console.log(bubbleSort(numArr));
+console.log(bubbleSort(largeNumArr));
